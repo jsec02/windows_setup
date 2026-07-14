9dfd44efb46871de5bf7c68ac1ed1266bf1c0bb9
master <justinbraun335@gmail.com>
2026-07-14 04:52:00 -0400

    feat: implement Set-KeyboardSettings

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,20 @@
 # ================================================================================
 # =                                  SETUP.PS1                                   =
 # ================================================================================
+
+function Set-KeyboardSettings {
+    $KeyboardSettings = @{
+        KeyboardDelay = 0
+        KeyboardSpeed = 31
+    }
+
+    $KeyboardSettings.GetEnumerator() | ForEach-Object {
+        Set-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name $_.Key -Value $_.Value
+    }
+}
+
+function Main {
+    Set-KeyboardSettings
+}
+
+Main
