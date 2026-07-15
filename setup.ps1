b15061300d6a33634bfa766f41de2036e5de53b5
master <justinbraun335@gmail.com>
2026-07-15 11:22:57 -0400

    feat: implement Set-TaskbarSettings

--- setup.ps1
+++ setup.ps1
@@ -4,12 +4,23 @@
 
 function Set-KeyboardSettings {
     $KeyboardSettings = @{
-        "KeyboardDelay" = 0
-        "KeyboardSpeed" = 31
+        'KeyboardDelay' = 0
+        'KeyboardSpeed' = 31
     }
 
     $KeyboardSettings.GetEnumerator() | ForEach-Object {
         Set-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name $_.Key -Value $_.Value
+    }
+}
+
+function Set-TaskbarSettings {
+    $TaskbarKeys = @(
+        'TaskbarAl',
+        'ShowTaskViewButton'
+    )
+
+    foreach ($Key in $TaskbarKeys) {
+        Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name $Key -Value 0
     }
 }
 
@@ -26,35 +37,35 @@ function Set-DarkMode {
 
 function Install-Programs {
     $Ids = @(
-        "7zip.7zip",
-        "Atuinsh.Atuin",
-        "Deskflow.Deskflow",
-        "Discord.Discord",
-        "Fastfetch-cli.Fastfetch",
-        "Git.Git",
-        "Microsoft.PowerToys",
-        "Microsoft.Powershell", 
-        "Microsoft.WSL",
-        "Mozilla.Firefox"
-        "Neovim.Neovim",
-        "Python.Python.3.14",
-        "RazerInc.RazerInstaller.Synapse4",
-        "Valve.Steam",
-        "VideoLAN.VLC",
-        "WiresharkFoundation.Wireshark",
-        "Zellij.Zellij",
-        "dbrgn.tealdeer",
-        "jeffvli.Feishin",
-        "qBittorrent.qBittorrent",
-        "sxyazi.yazi",
-        "yt-dlp.yt-dlp"
+        '7zip.7zip',
+        'Atuinsh.Atuin',
+        'Deskflow.Deskflow',
+        'Discord.Discord',
+        'Fastfetch-cli.Fastfetch',
+        'Git.Git',
+        'Microsoft.PowerToys',
+        'Microsoft.Powershell', 
+        'Microsoft.WSL',
+        'Mozilla.Firefox'
+        'Neovim.Neovim',
+        'Python.Python.3.14',
+        'RazerInc.RazerInstaller.Synapse4',
+        'Valve.Steam',
+        'VideoLAN.VLC',
+        'WiresharkFoundation.Wireshark',
+        'Zellij.Zellij',
+        'dbrgn.tealdeer',
+        'jeffvli.Feishin',
+        'qBittorrent.qBittorrent',
+        'sxyazi.yazi',
+        'yt-dlp.yt-dlp'
     )
 
     & winget install $Ids
 }
 
 function Update-Path {
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    $env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path','User')
 }
 
 function Initialize-TLDR {
@@ -66,11 +77,11 @@ function Initialize-WSL {
 }
 
 function Read-RestartConfirmation {
-    Write-Host "Windows setup has completed. Registry changes require a restart."
-    $RestartConfirmed = (Read-Host "Would you like to restart now? (y/n)").Trim().ToLower()
+    Write-Host 'Windows setup has completed. Registry changes require a restart.'
+    $RestartConfirmed = (Read-Host 'Would you like to restart now? (y/n)').Trim().ToLower()
 
-    if ($RestartConfirmed -eq "y") {
-        Write-Host "Restarting now..."
+    if ($RestartConfirmed -eq 'y') {
+        Write-Host 'Restarting now...'
         Start-Sleep -Seconds 2
         Restart-Computer
     }
